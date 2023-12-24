63693b701094c146ecf5aeb44f45d2394570093e
yunhwan <yhyoon1004@naver.com>
2023-12-24 19:52:10 +0900

    기사작성 페이지 기사 요소 추가

--- app/write/page.js
+++ app/write/page.js
@@ -1,7 +1,18 @@
 'use client'
 
 import ReactQuill from "react-quill";
-import {Col, Container, Form, InputGroup, ListGroup, ListGroupItem, Row} from "react-bootstrap";
+import {
+    Button,
+    Col,
+    Container,
+    Form,
+    FormControl,
+    FormLabel, FormSelect,
+    InputGroup,
+    ListGroup,
+    ListGroupItem,
+    Row
+} from "react-bootstrap";
 import {useEffect, useState} from "react";
 
 export default function ArticleWriter() {
@@ -71,7 +82,53 @@ export default function ArticleWriter() {
                 <Col md={4}>
                     <ListGroup>
                         <ListGroupItem>
-                            도구 영역
+                            <Row className={"mb-1"}>
+                                <Col>
+                                    Category (카테고리)
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <FormSelect aria-label="Default select example">
+                                        <option>기사 분야</option>
+                                        <option value="1">정치</option>
+                                        <option value="2">경제</option>
+                                        <option value="3">산업</option>
+                                        <option value="3">사회</option>
+                                        <option value="3">전국</option>
+                                        <option value="3">세계</option>
+                                        <option value="3">연예</option>
+                                        <option value="3">스포츠</option>
+                                    </FormSelect>
+                                </Col>
+
+                            </Row>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <FormLabel>기사내용 요약</FormLabel>
+                            <FormControl style={{minHeight:"200px"}} as={"textarea"} placeholder={"500자 내외"}/>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <FormLabel>Keyword (키워드)</FormLabel>
+                            <FormControl placeholder={"',' 구분 -> 누리호,위성발사"}/>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <FormLabel>Byline (바이라인)</FormLabel>
+                            <FormControl placeholder={"작성자 기자명 -> 홍길동"}/>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <Row>
+                                <Col>
+                                    <Button className={"me-1"} variant="secondary">임시 저장</Button>
+                                    <Button variant="success" >예약 송고</Button>
+                                </Col>
+                            </Row>
+                            <Row className={"mt-1"}>
+                                <Col>
+                                    <Button className={"me-1"} variant="danger">작성 취소</Button>
+                                    <Button >기사 송고</Button>
+                                </Col>
+                            </Row>
                         </ListGroupItem>
                     </ListGroup>
                 </Col>
